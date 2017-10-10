cf1c54d1b09c3f05567b3d558c14c51444d69e3f
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-10 14:38:59 +0300

    setup.py filled

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import setuptools
 
-import sys
-if not sys.version_info[0] == 3:
-    sys.exit("Sorry, only python version 3 is supported")
-
 setuptools.setup(
     name='pydmt',
     version='0.0.1',
     description='pydmt is a tool for constructing software',
     long_description='pydmt is a tool for constructing software',
-    url='https://veltzer.github.io/pydmt',
+    url='https://github.com/veltzer/pydmt',
+    download_url='https://github.com/veltzer/pydmt',
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
+    maintainer='Mark Veltzer',
+    maintainer_email='mark.veltzer@gmail.com',
     license='MIT',
+    platforms=['python3'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
