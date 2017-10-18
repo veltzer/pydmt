aaa366198ba79f7738631a04ae0760c53a8735de
Mark Veltzer <mark.veltzer@gmail.com>
2017-10-18 11:40:34 +0300

    not copying

--- pydmt/core/pydmt.py
+++ pydmt/core/pydmt.py
@@ -2,9 +2,31 @@ from typing import List, Dict
 
 import shutil
 
+import os
+
 from pydmt.api.builder import Builder
 from pydmt.core.cache import Cache
 from pydmt.core.utils import sha1_file
+
+
+class BuildProcessStats:
+    def __init__(self):
+        self.builder = 0
+        self.copy = 0
+        self.nop = 0
+        self.missing = 0
+
+    def add_builder(self):
+        self.builder += 1
+
+    def add_copy(self):
+        self.copy += 1
+
+    def add_nop(self):
+        self.nop += 1
+
+    def add_missing(self):
+        self.missing += 1
 
 
 class PyDMT:
@@ -13,14 +35,26 @@ class PyDMT:
         self.target_to_builder = {}  # type: Dict[str, Builder]
         self.cache = Cache()
 
-    def build_by_builder(self, b: Builder) -> None:
-        """ run one builder, return weather we ran it or not """
+    def build_by_builder(self, b: Builder, stats: BuildProcessStats):
+        """ run one builder, return statistics about the run """
         target_signature = b.get_signature()
         blob_name = self.cache.get_list_by_signature(target_signature)
         if blob_name:
-            for object_name, ref in Cache.iterate_objects(blob_name):
-                shutil.copy(ref, object_name)
+            for object_name, signature in Cache.iterate_objects(blob_name):
+                filename = self.cache.get_object_by_signature(signature)
+                if os.path.isfile(object_name):
+                    object_name_signature = sha1_file(object_name)
+                    if object_name_signature != signature:
+                        shutil.copy(filename, object_name)
+                        stats.add_copy()
+                    else:
+                        stats.add_nop()
+                else:
+                    stats.add_missing()
+                    shutil.copy(filename, object_name)
+                    stats.add_copy()
         else:
+            stats.add_builder()
             b.build()
             # first lets build a list of what was constructed
             targets = b.get_targets()
@@ -34,13 +68,13 @@ class PyDMT:
 
             self.cache.save_list_by_signature(target_signature, content)
 
-    def build_by_target(self, target: str) -> None:
+    def build_by_target(self, target: str, stats: BuildProcessStats) -> None:
         b = self.target_to_builder[target]
-        self.build_by_builder(b)
+        self.build_by_builder(b, stats)
 
-    def build_by_targets(self, targets: List[str]) -> None:
+    def build_by_targets(self, targets: List[str], stats: BuildProcessStats) -> None:
         for target in targets:
-            self.build_by_target(target)
+            self.build_by_target(target, stats)
 
     def addBuilder(self, b: Builder) -> None:
         self.builders.append(b)
--- tests/unit_tests/test_all.py
+++ tests/unit_tests/test_all.py
@@ -2,7 +2,9 @@ import unittest
 
 import shutil
 
-import pydmt.core.pydmt
+import os
+
+from pydmt.core.pydmt import PyDMT, BuildProcessStats
 from pydmt.builders.copy import Copy
 from pydmt.core.tempdir import tempdir
 
@@ -10,18 +12,47 @@ from pydmt.core.tempdir import tempdir
 class TestAll(unittest.TestCase):
 
     def testCreateObject(self):
-        pydmt.core.pydmt.PyDMT()
+        PyDMT()
 
     def testCreateBuild(self):
-        p = pydmt.core.pydmt.PyDMT()
-        p.build_by_targets([])
+        p = PyDMT()
+        p.build_by_targets([], BuildProcessStats())
 
     def testCheckSimpleCopy(self):
         with tempdir():
             shutil.copy("/etc/passwd", "passwd")
-            p = pydmt.core.pydmt.PyDMT()
+            p = PyDMT()
             b = Copy("passwd", "copy_of_passwd")
             p.addBuilder(b)
-            p.build_by_builder(b)
+            stats = BuildProcessStats()
+            p.build_by_builder(b, stats)
+            self.assertEqual(stats.builder, 1)
+            self.assertEqual(stats.copy, 0)
 
+    def testCheckIncremental(self):
+        with tempdir():
+            shutil.copy("/etc/passwd", "passwd")
+            p = PyDMT()
+            b = Copy("passwd", "copy_of_passwd")
+            p.addBuilder(b)
+            p.build_by_builder(b, BuildProcessStats())
+            stats = BuildProcessStats()
+            p.build_by_builder(b, stats)
+            self.assertEqual(stats.builder, 0)
+            self.assertEqual(stats.copy, 0)
+            self.assertEqual(stats.nop, 1)
 
+    def testCheckCopyAfterRemove(self):
+        with tempdir():
+            shutil.copy("/etc/passwd", "passwd")
+            p = PyDMT()
+            b = Copy("passwd", "copy_of_passwd")
+            p.addBuilder(b)
+            p.build_by_builder(b, BuildProcessStats())
+            stats = BuildProcessStats()
+            os.unlink("copy_of_passwd")
+            p.build_by_builder(b, stats)
+            self.assertEqual(stats.builder, 0)
+            self.assertEqual(stats.copy, 1)
+            self.assertEqual(stats.nop, 0)
+            self.assertEqual(stats.missing, 1)
