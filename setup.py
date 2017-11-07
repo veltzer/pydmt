3e49f70ba3c9924754eaac113cdb4b7568bf38cd
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-07 14:14:08 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pydmt',
-    version='0.0.12',
+    version='0.0.13',
     description='pydmt is a tool for constructing software',
     long_description='pydmt is a tool for constructing software',
     url='https://github.com/veltzer/pydmt',
