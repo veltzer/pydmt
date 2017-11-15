4799db6c88ada0f71b07606f4ac734f201233e08
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-15 14:53:38 +0200

    added sphinx builder

---
+++ pydmt/builders/sphinx.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+from typing import List
+
+from pydmt.api.builder import Builder
+from pydmt.core.utils import sha1_files_folders, sha1_folders, files_under_folder, unlink_files
+import subprocess
+
+
+class Sphinx(Builder):
+    def __init__(self,
+                 package_name: str,
+                 source_folder: str = "sphinx",
+                 target_folder: str = "docs"):
+        super().__init__()
+        self.package_name = package_name
+        self.source_folder = source_folder
+        self.target_folder = target_folder
+
+    def get_signature(self) -> str:
+        if os.path.isfile(self.package_name):
+            return sha1_files_folders(
+                files=[self.package_name],
+                folders=[self.source_folder],
+            )
+        if os.path.isdir(self.package_name):
+            return sha1_folders(folders=[self.source_folder, self.package_name])
+
+    def _get_source_folder_targets(self) -> List[str]:
+        return [
+            os.path.join(self.source_folder, "modules.rst"),
+            os.path.join(self.source_folder, "{}.rst".format(self.package_name)),
+            os.path.join(self.source_folder, "{}.scripts.rst".format(self.package_name)),
+        ]
+
+    def build(self) -> None:
+        unlink_files(self._get_source_folder_targets(), only_if_exist=True)
+        args = [
+            "sphinx-apidoc",
+            "-f",
+            "-o",
+            self.source_folder,
+            self.package_name,
+        ]
+        subprocess.check_call(args)
+        if os.path.isdir(self.target_folder):
+            shutil.rmtree(self.target_folder, ignore_errors=False)
+        subprocess.check_call([
+            "sphinx-build",
+            self.source_folder,
+            self.target_folder,
+        ])
+
+    def get_targets(self) -> List[str]:
+        return []
+
+    def get_targets_post_build(self) -> List[str]:
+        return_list = self._get_source_folder_targets()
+        return_list.extend(files_under_folder(self.target_folder))
+        return return_list
--- pydmt/core/utils.py
+++ pydmt/core/utils.py
@@ -1,21 +1,31 @@
 import hashlib
 import os
 import shutil
+from typing import List
 
 
-def hex_digest(filename: str, algorithm_name: str) -> str:
+def hex_digest_files(filenames: List[str], algorithm_name: str) -> str:
     block_size = 65536
     hash_object = hashlib.new(algorithm_name)
-    with open(filename, 'rb') as file_handle:
-        buf = file_handle.read(block_size)
-        while len(buf) > 0:
-            hash_object.update(buf)
+    for filename in filenames:
+        with open(filename, 'rb') as file_handle:
             buf = file_handle.read(block_size)
+            while len(buf) > 0:
+                hash_object.update(buf)
+                buf = file_handle.read(block_size)
     return hash_object.hexdigest()
 
 
+def hex_digest_file(filename: str, algorithm_name: str) -> str:
+    return hex_digest_files(filenames=[filename], algorithm_name=algorithm_name)
+
+
 def sha1_file(filename: str) -> str:
-    return hex_digest(filename, "sha1")
+    return hex_digest_file(filename=filename, algorithm_name="sha1")
+
+
+def sha1_files(filenames: List[str]) -> str:
+    return hex_digest_files(filenames=filenames, algorithm_name="sha1")
 
 
 def makedirs_for_file(filename: str):
@@ -28,3 +38,34 @@ def copy_mkdir(source: str, destination: str):
     makedirs_for_file(destination)
     shutil.copy(source, destination)
 
+
+def unlink_files(files: List[str], only_if_exist: bool=True) -> None:
+    for file in files:
+        if only_if_exist and os.path.isfile(file):
+            os.unlink(file)
+        else:
+            os.unlink(file)
+
+
+def files_under_folder(folder: str) -> List[str]:
+    f = []
+    for (_, _, filenames) in os.walk(folder):
+        f.extend(filenames)
+    return f
+
+
+def files_under_folders(folders: List[str]) -> List[str]:
+    f = []
+    for folder in folders:
+        f.extend(files_under_folder(folder))
+    return f
+
+
+def sha1_folders(folders: List[str]) -> str:
+    return sha1_files(filenames=files_under_folders(folders=folders))
+
+
+def sha1_files_folders(files: List[str], folders: List[str]) -> str:
+    f = files
+    f.extend(files_under_folders(folders=folders))
+    return sha1_files(f)
