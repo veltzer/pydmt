180184b11c3230cccc42f8cef16ac7fa3823e5c6
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-27 15:44:16 +0200

    new versino with deps

--- pydmt/features/templating.py
+++ pydmt/features/templating.py
@@ -3,14 +3,22 @@ import os
 from pydmt.api.feature import Feature
 from pydmt.builders.mako import Mako
 from pydmt.core.pydmt import PyDMT
+from pydmt.core.utils import files_under_folder
 
 
 class Templating(Feature):
-    def __init__(self, data=None):
+    def __init__(
+            self,
+            data=None,
+            templates_folder: str="templates",
+            config_folder: str="config",
+    ):
         self.data = data
+        self.templates_folder = templates_folder
+        self.config_folder = config_folder
 
     def setup(self, pydmt: PyDMT) -> None:
-        for root, directories, filenames in os.walk("templates"):
+        for root, directories, filenames in os.walk(self.templates_folder):
             for filename in filenames:
                 source = os.path.join(root, filename)
                 target_base, ext = os.path.splitext(source)
@@ -20,5 +28,6 @@ class Templating(Feature):
                         source=source,
                         target=target,
                         data=self.data,
+                        dep_files=files_under_folder(self.config_folder),
                     )
                     pydmt.add_builder(builder)
