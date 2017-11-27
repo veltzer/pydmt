dd8af36d7ba94d8befa0f2c072bb685ad67281b5
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-27 15:12:55 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 # noinspection PyPep8
 setuptools.setup(
     name='pydmt',
-    version='0.0.38',
+    version='0.0.40',
     description='python dependency management tool',
     long_description='python dependency management tool',
     author='Mark Veltzer',
@@ -20,5 +20,5 @@ setuptools.setup(
     install_requires=['click', 'pyfakeuse', 'pylogconf', 'mako', 'Sphinx'],
     classifiers=['Development Status :: 4 - Beta', 'Environment :: Console', 'Operating System :: OS Independent', 'Programming Language :: Python', 'Programming Language :: Python :: 3', 'Topic :: Utilities'],
     data_files=[],
-    entry_points={'console_scripts': ['pydmt_build=pydmt.scripts.pydmt_build:main']},
+    entry_points={'console_scripts': ['pydmt_build=pydmt.scripts.build:main']},
 )
