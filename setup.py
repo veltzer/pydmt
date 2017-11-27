fb3fb9b562167a86e7ca8df49b1183cb7df60a91
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-27 15:23:38 +0200

    new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 # noinspection PyPep8
 setuptools.setup(
     name='pydmt',
-    version='0.0.41',
+    version='0.0.43',
     description='python dependency management tool',
     long_description='python dependency management tool',
     author='Mark Veltzer',
