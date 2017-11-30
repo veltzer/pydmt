867814a49bf2f8928caf9f8f066ce84ef488b2db
Mark Veltzer <mark.veltzer@gmail.com>
2017-11-30 15:49:52 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pydmt',
-    version='0.0.46',
+    version='0.0.47',
     description='python dependency management tool',
     long_description='python dependency management tool',
     author='Mark Veltzer',
