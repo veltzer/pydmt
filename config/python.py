465ef391b0fc2670a827d7cb71197578ecf2e0f3
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-02 22:48:10 +0200

    better template

--- config/python.py
+++ config/python.py
@@ -1,8 +1,14 @@
+import config.project
+
+package_name = config.project.project_name
+
 console_scripts = [
     'pydmt_build=pydmt.scripts.build:main',
 ]
+
 setup_requires = [
 ]
+
 install_requires = [
     # core
     'click',  # for command line parsing
@@ -12,8 +18,11 @@ install_requires = [
     'mako',  # for template handling
     'Sphinx',  # for the sphinx builder
     ]
+
 dev_requires = [
     'pypitools',  # for upload and registration
     'pydmt',  # for building
     'pyclassifiers',  # for specifying classifications
 ]
+
+python_requires = ">=3"
--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 import setuptools
 
+"""
+The documentation can be found at:
+http://setuptools.readthedocs.io/en/latest/setuptools.html
+"""
 setuptools.setup(
+    # the first three fields are a must according to the documentation
     name='pydmt',
     version='0.0.48',
+    packages=[
+        'pydmt',
+        'pydmt.core',
+        'pydmt.builders',
+        'pydmt.utils',
+        'pydmt.scripts',
+        'pydmt.api',
+        'pydmt.features',
+    ],
+    # from here all is optional
     description='python dependency management tool',
     long_description='python dependency management tool',
     author='Mark Veltzer',
@@ -10,39 +25,39 @@ setuptools.setup(
     maintainer='Mark Veltzer',
     maintainer_email='mark.veltzer@gmail.com',
     keywords=[
-        "pydmt",
-        "cons",
-        "scons",
-        "software construction",
-        "make",
-        "maven",
-        "mvn",
+        'pydmt',
+        'cons',
+        'scons',
+        'software construction',
+        'make',
+        'maven',
+        'mvn',
     ],
     url='https://veltzer.github.io/pydmt',
     download_url='https://github.com/veltzer/pydmt',
     license='MIT',
     platforms=[
-        "python3",
+        'python3',
     ],
-    packages=setuptools.find_packages(),
     install_requires=[
-        "click",
-        "pyfakeuse",
-        "pylogconf",
-        "mako",
-        "Sphinx",
+        'click',
+        'pyfakeuse',
+        'pylogconf',
+        'mako',
+        'Sphinx',
     ],
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Utilities",
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
     ],
     data_files=[
     ],
     entry_points={'console_scripts': [
-        "pydmt_build=pydmt.scripts.build:main",
+        'pydmt_build=pydmt.scripts.build:main',
     ]},
+    python_requires='>=3',
 )
