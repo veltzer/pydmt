ee6212abc6909bc8d1631f811a72cb763401cff5
Mark Veltzer <mark.veltzer@gmail.com>
2017-12-02 22:48:44 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ http://setuptools.readthedocs.io/en/latest/setuptools.html
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pydmt',
-    version='0.0.48',
+    version='0.0.49',
     packages=[
         'pydmt',
         'pydmt.core',
