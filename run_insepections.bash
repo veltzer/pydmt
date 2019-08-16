bd91cc9e84631f5305d023c9b293eea5c6c47bb4
Mark Veltzer <mark.veltzer@gmail.com>
2019-08-17 02:03:44 +0300

    fixed script

--- run_insepections.bash
+++ run_insepections.bash
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -n "${PYCHARM_HOME}" ]
 then
-	${PYCHARM_HOME}/bin/inspect.sh . .idea/inspectionProfiles/profiles_settings.xml inspections
+	${PYCHARM_HOME}/bin/inspect.sh $PWD .idea/inspectionProfiles/profiles_settings.xml inspections
 else
 	echo "varible 'PYCHARM_HOME' is not set"
 fi
