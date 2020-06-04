d4222847a7d9997b4afb53f8defeab692d9bdba6
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-04 16:52:06 +0300

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name='pydmt',
-    version='0.0.68',
+    version='0.0.69',
     packages=[
         'pydmt',
         'pydmt.api',
