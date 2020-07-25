fe38d6ddd274a3b61a0abbd542af158aaeaa7bd6
Mark Veltzer <mark.veltzer@gmail.com>
2020-07-25 16:37:58 +0300

    import of config delayed to run time

--- pydmt/features/documentation.py
+++ pydmt/features/documentation.py
@@ -3,7 +3,6 @@ import os
 from pydmt.api.feature import Feature
 from pydmt.builders.sphinx import Sphinx
 from pydmt.core.pydmt import PyDMT
-import config.project
 
 
 class Documentation(Feature):
@@ -12,8 +11,10 @@ class Documentation(Feature):
         sphinx_folder: str = "sphinx",
     ):
         self.sphinx_folder = sphinx_folder
+        import config.project
+        self.project_name = config.project.project_name
 
     def setup(self, pydmt: PyDMT) -> None:
         if not os.path.isdir(self.sphinx_folder):
             return
-        pydmt.add_builder(Sphinx(package_name=config.project.project_name))
+        pydmt.add_builder(Sphinx(package_name=self.project_name))
