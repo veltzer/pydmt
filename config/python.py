682d49445f70fed0eb256193fadaef512d0ff525
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-15 02:29:53 +0300

    added yaml json schema validation

--- config/python.py
+++ config/python.py
@@ -23,6 +23,8 @@ install_requires = [
     'pytconf',
     'mako',
     'Sphinx',
+    'pyyaml',
+    'jsonschema',
 ]
 
 dev_requires = [
@@ -36,6 +38,7 @@ python_requires = ">=3.7"
 
 extras_require = {
 }
+
 test_os = "[ubuntu-18.04, ubuntu-20.04]"
 test_python = "[3.7, 3.8, 3.9]"
 test_container = "[ 'ubuntu:18.04', 'ubuntu:20.04' ]"
