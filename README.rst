fc235290698d4f314df6f2371a3f7793ba630ec4
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-15 02:19:56 +0300

    fixed mkdir bug and new version

--- README.rst
+++ README.rst
@@ -12,6 +12,6 @@ project website: https://veltzer.github.io/pydmt
 
 author: Mark Veltzer
 
-version: 0.0.81
+version: 0.0.82
 
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pydmt",
-    version="0.0.81",
+    version="0.0.82",
     packages=[
         'pydmt',
         'pydmt.api',
