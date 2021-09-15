f177f7a7097f158c3f9bf74abf5f5c85d608a9d1
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-15 09:03:42 +0300

    added json and yaml validation options

--- pydmt/builders/yaml.py
+++ pydmt/builders/yaml.py
@@ -8,7 +8,8 @@ from pydmt.utils.filesystem import mkdir_touch
 from pydmt.builders.one_source_one_target import OneSourceOneTarget
 
 METADATA = "metadata"
-SCHEMA_FILE = "schema_file"
+SCHEMA_FILE_JSON = "schema_file_json"
+SCHEMA_FILE_YAML = "schema_file_yaml"
 
 
 class YamlValidate(OneSourceOneTarget):
@@ -16,8 +17,15 @@ class YamlValidate(OneSourceOneTarget):
         with open(self.source) as yaml_stream:
             data = yaml.load(yaml_stream, yaml.SafeLoader)
             if METADATA in data:
-                schema_file = data[METADATA][SCHEMA_FILE]
-                with open(schema_file) as schema_stream:
-                    schema = json.load(schema_stream)
-                    validate(data, schema)
+                metadata = data[METADATA]
+                if SCHEMA_FILE_JSON in metadata:
+                    schema_file = data[METADATA][SCHEMA_FILE_JSON]
+                    with open(schema_file) as schema_stream:
+                        schema = json.load(schema_stream)
+                        validate(data, schema)
+                if SCHEMA_FILE_YAML in metadata:
+                    schema_file = data[METADATA][SCHEMA_FILE_YAML]
+                    with open(schema_file) as schema_stream:
+                        schema = yaml.load(schema_stream, yaml.SafeLoader)
+                        validate(data, schema)
             mkdir_touch(self.target)
