afe35dd4f7ef0a547f90e726d971d1c96dff5f3b
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-15 07:36:28 +0300

    json bug, new version

--- README.rst
+++ README.rst
@@ -12,6 +12,6 @@ project website: https://veltzer.github.io/pydmt
 
 author: Mark Veltzer
 
-version: 0.0.84
+version: 0.0.85
 
 
--- pydmt/static.py
+++ pydmt/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.84"
+VERSION_STR = "0.0.85"
 DESCRIPTION = "python dependency management tool"
 APP_NAME = "pydmt"
 LOGGER_NAME = "pydmt"
