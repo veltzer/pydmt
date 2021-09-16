bb6fff171ed47837debec4c384e46d288bed227a
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-17 00:45:02 +0300

    added url handling

--- pydmt/builders/yaml.py
+++ pydmt/builders/yaml.py
@@ -1,4 +1,5 @@
 import json
+from urllib.request import urlopen
 import yaml
 
 from jsonschema import validate
@@ -10,6 +11,8 @@ from pydmt.builders.one_source_one_target import OneSourceOneTarget
 METADATA = "metadata"
 SCHEMA_FILE_JSON = "schema_file_json"
 SCHEMA_FILE_YAML = "schema_file_yaml"
+SCHEMA_URL_JSON = "schema_url_json"
+SCHEMA_URL_YAML = "schema_url_yaml"
 
 
 class YamlValidate(OneSourceOneTarget):
@@ -27,5 +30,15 @@ class YamlValidate(OneSourceOneTarget):
                     schema_file = data[METADATA][SCHEMA_FILE_YAML]
                     with open(schema_file) as schema_stream:
                         schema = yaml.load(schema_stream, yaml.SafeLoader)
+                        validate(data, schema)
+                if SCHEMA_URL_JSON in metadata:
+                    schema_url = data[METADATA][SCHEMA_URL_JSON]
+                    with urlopen(schema_url) as f:
+                        schema = json.load(f.read())
+                        validate(data, schema)
+                if SCHEMA_URL_YAML in metadata:
+                    schema_url = data[METADATA][SCHEMA_URL_YAML]
+                    with urlopen(schema_url) as f:
+                        schema = json.load(f.read())
                         validate(data, schema)
             mkdir_touch(self.target)
