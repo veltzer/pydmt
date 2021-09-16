95abed32bf3d00a937ab0271a33894179996b6f5
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-17 00:55:30 +0300

    more fixes

--- pydmt/builders/yaml.py
+++ pydmt/builders/yaml.py
@@ -18,7 +18,7 @@ SCHEMA_URL_YAML = "schema_url_yaml"
 class YamlValidate(OneSourceOneTarget):
     def build(self):
         with open(self.source) as yaml_stream:
-            data = yaml.load(yaml_stream, yaml.SafeLoader)
+            data = yaml.safe_load(yaml_stream)
             if METADATA in data:
                 metadata = data[METADATA]
                 if SCHEMA_FILE_JSON in metadata:
@@ -29,16 +29,16 @@ class YamlValidate(OneSourceOneTarget):
                 if SCHEMA_FILE_YAML in metadata:
                     schema_file = data[METADATA][SCHEMA_FILE_YAML]
                     with open(schema_file) as schema_stream:
-                        schema = yaml.load(schema_stream, yaml.SafeLoader)
+                        schema = yaml.safe_load(schema_stream)
                         validate(data, schema)
                 if SCHEMA_URL_JSON in metadata:
                     schema_url = data[METADATA][SCHEMA_URL_JSON]
                     with urlopen(schema_url) as f:
-                        schema = json.load(f.read().decode())
+                        schema = json.loads(f.read())
                         validate(data, schema)
                 if SCHEMA_URL_YAML in metadata:
                     schema_url = data[METADATA][SCHEMA_URL_YAML]
                     with urlopen(schema_url) as f:
-                        schema = json.load(f.read().decode())
+                        schema = yaml.safe_load(f.read())
                         validate(data, schema)
             mkdir_touch(self.target)
