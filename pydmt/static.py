6af57e4e20f2e216906654ca4f9042058bc2cefd
Mark Veltzer <mark.veltzer@gmail.com>
2021-09-17 02:02:12 +0300

    DONE notes

--- pydmt/static.py
+++ pydmt/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.88"
+VERSION_STR = "0.0.89"
 DESCRIPTION = "python dependency management tool"
 APP_NAME = "pydmt"
 LOGGER_NAME = "pydmt"
