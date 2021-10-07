374dc29ccf3f8230808eeb02cf8b656de8857849
Mark Veltzer <mark.veltzer@gmail.com>
2021-10-07 08:18:03 +0300

    fix for yaml check and new version

--- pydmt/builders/yaml.py
+++ pydmt/builders/yaml.py
@@ -17,16 +17,17 @@ class YamlValidate(OneSourceOneTarget):
     def build(self):
         with open(self.source) as yaml_stream:
             data = yaml.safe_load(yaml_stream)
-            if METADATA in data:
-                metadata = data[METADATA]
-                if SCHEMA_URL_JSON in metadata:
-                    schema_url = data[METADATA][SCHEMA_URL_JSON]
-                    with urlopen(schema_url) as f:
-                        schema = json.loads(f.read())
-                        validate(data, schema)
-                if SCHEMA_URL_YAML in metadata:
-                    schema_url = data[METADATA][SCHEMA_URL_YAML]
-                    with urlopen(schema_url) as f:
-                        schema = yaml.safe_load(f.read())
-                        validate(data, schema)
+            if isinstance(data, dict):
+                if METADATA in data:
+                    metadata = data[METADATA]
+                    if SCHEMA_URL_JSON in metadata:
+                        schema_url = data[METADATA][SCHEMA_URL_JSON]
+                        with urlopen(schema_url) as f:
+                            schema = json.loads(f.read())
+                            validate(data, schema)
+                    if SCHEMA_URL_YAML in metadata:
+                        schema_url = data[METADATA][SCHEMA_URL_YAML]
+                        with urlopen(schema_url) as f:
+                            schema = yaml.safe_load(f.read())
+                            validate(data, schema)
             mkdir_touch(self.target)
