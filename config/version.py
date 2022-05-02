48bec54b8a2696359226313d86ffe0b2498150c3
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-02 06:32:11 +0300

    new version

--- config/version.py
+++ config/version.py
@@ -1,2 +1,2 @@
-version = (0, 0, 98)
+version = (0, 0, 99)
 version_str = ".".join(str(x) for x in version)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pydmt",
-    version="0.0.98",
+    version="0.0.99",
     packages=[
         'pydmt',
         'pydmt.api',
