e16a4b2b941f9bec4cefef7f55a150730dfbc8a7
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-02 12:50:21 +0300

    added npm installer

---
+++ pydmt/features/npm.py
@@ -0,0 +1,14 @@
+import os.path
+
+from pydmt.api.feature import Feature
+from pydmt.builders.npm import Installer
+from pydmt.core.pydmt import PyDMT
+
+
+class Npm(Feature):
+    def setup(self, pydmt: PyDMT) -> None:
+        if os.path.isfile("package.json"):
+            pydmt.add_builder(Installer(
+                source="package.json",
+                target="out/npm.stamp",
+            ))
