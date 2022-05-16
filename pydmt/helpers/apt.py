56e59221e594b2142d6bf83c8ff7bcb0179693b5
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-16 23:15:12 +0300

    more stuff

--- pydmt/helpers/apt.py
+++ pydmt/helpers/apt.py
@@ -21,3 +21,7 @@ pack_list = glob.glob(
 pack_str = " ".join(pack_list)
 release_id = subprocess.check_output(["lsb_release", "--id", "--short"]).decode().rstrip()
 key_file = "public_key.gpg"
+
+personal_slug = 'veltzer'
+exception = f'50{personal_slug}'
+apache_site_file = f'{personal_slug}.apt'
