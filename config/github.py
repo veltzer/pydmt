2139399fd085ea9aea8570b57ae2a84708ea9a02
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-16 21:13:11 +0300

    moved to new paradigm

--- config/github.py
+++ config/github.py
@@ -1,2 +1,7 @@
-workflows_os = ["ubuntu-22.04"]
-workflows_python = ["3.10"]
+workflows_platforms = [
+        ("ubuntu-22.04", "3.10"),
+        ("ubuntu-20.04", "3.9"),
+        ("ubuntu-20.04", "3.8"),
+        ("ubuntu-20.04", "3.7"),
+        ("ubuntu-20.04", "3.6"),
+]
