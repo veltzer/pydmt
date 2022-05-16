540802b1135a27c87137ca9343a8c71367b4f52d
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-16 22:52:25 +0300

    new version

--- pydmt/helpers/apt.py
+++ pydmt/helpers/apt.py
@@ -4,20 +4,20 @@ import subprocess
 
 import pydmt.helpers.general
 
-apt_protocol = "https"
-apt_codename = subprocess.check_output(["lsb_release", "--codename", "--short"]).decode().rstrip()
-apt_arch = subprocess.check_output(
+protocol = "https"
+codename = subprocess.check_output(["lsb_release", "--codename", "--short"]).decode().rstrip()
+arch = subprocess.check_output(
     "dpkg-architecture | grep -e ^DEB_BUILD_ARCH= | cut -d = -f 2", shell=True
 ).decode().rstrip()
-apt_architectures = f"{apt_arch} source"
-apt_component = "main"
-apt_folder = "apt"
-apt_service_dir = os.path.join(
-    pydmt.helpers.general.homedir, "public_html/public", apt_folder
+architectures = f"{arch} source"
+component = "main"
+folder = "apt"
+service_dir = os.path.join(
+    pydmt.helpers.general.homedir, "public_html/public", folder
 )
-apt_pack_list = glob.glob(
+pack_list = glob.glob(
     os.path.join(pydmt.helpers.general.homedir, "packages", "*.deb")
 )
-apt_pack_str = " ".join(apt_pack_list)
-apt_id = subprocess.check_output(["lsb_release", "--id", "--short"]).decode().rstrip()
-apt_key_file = "public_key.gpg"
+pack_str = " ".join(pack_list)
+release_id = subprocess.check_output(["lsb_release", "--id", "--short"]).decode().rstrip()
+key_file = "public_key.gpg"
