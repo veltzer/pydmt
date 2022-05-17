a265e9ed0b923de4b4316752f0b2f30b1175e107
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-17 06:43:30 +0300

    more stuff

--- pydmt/helpers/signature.py
+++ pydmt/helpers/signature.py
@@ -1,9 +1,13 @@
 import datetime
 
 
-def get_copyright_years(project_year_started):
-    # return ", ".join(map(str, range(int(project_year_started), datetime.datetime.now().year + 1)))
+def get_copyright_years_long(project_year_started):
     general_current_year = datetime.datetime.now().year
-    if str(general_current_year) == project_year_started:
+    return ", ".join(map(str, range(int(project_year_started), general_current_year)))
+
+
+def get_copyright_years_short(project_year_started):
+    general_current_year = datetime.datetime.now().year
+    if general_current_year == project_year_started:
         return general_current_year
     return f"{project_year_started}-{general_current_year}"
