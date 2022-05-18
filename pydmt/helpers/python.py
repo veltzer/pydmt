b9cc326c06301e105bc6b3e419c8ea7f95f96141
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:32:27 +0300

    added stuff to add

--- pydmt/helpers/python.py
+++ pydmt/helpers/python.py
@@ -95,3 +95,20 @@ def get_list_quoted(a_list: List[str]) -> str:
     s += ", ".join(quoted)
     s += "]"
     return s
+
+# stuff to add follows
+license = "MIT"
+platforms = [
+    "python3",
+]
+classifiers = [
+    pyclassifiers.values.DevelopmentStatus__4_Beta,
+    pyclassifiers.values.Environment__Console,
+    pyclassifiers.values.OperatingSystem__OSIndependent,
+    pyclassifiers.values.ProgrammingLanguage__Python,
+    pyclassifiers.values.ProgrammingLanguage__Python__3,
+    pyclassifiers.values.ProgrammingLanguage__Python__3__Only,
+    pyclassifiers.values.ProgrammingLanguage__Python__39,
+    pyclassifiers.values.Topic__Utilities,
+    pyclassifiers.values.License__OSIApproved__MITLicense,
+]
---
+++ pydmt/helpers/urls.py
@@ -0,0 +1,7 @@
+launchpad_username = "mark-veltzer"
+github_username = "veltzer"
+website = f"https://{github_username}.github.io/{name}"
+website_source = f"https://github.com/{github_username}/{name}"
+website_git = f"git://github.com/{github_username}/{name}.git"
+website_download_ppa = "https://launchpanet/~{launchpad_username}/+archive/ubuntu/ppa"
+website_download_src = website_source
