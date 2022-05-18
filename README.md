d8df6cd3e152e462fb86111b96aa9f5cc2ff242c
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 11:21:49 +0300

    new version

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ project website: https://veltzer.github.io/pydmt
 
 author: Mark Veltzer
 
-version: 0.1.51
+version: 0.1.52
 
 ## build status
 
--- pydmt/static.py
+++ pydmt/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.1.51"
+VERSION_STR = "0.1.52"
 DESCRIPTION = "python dependency management tool"
 APP_NAME = "pydmt"
 LOGGER_NAME = "pydmt"
