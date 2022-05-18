d3845daaa8314001fd2d92653c03526ba0dc4ffb
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 17:23:17 +0300

    added project helper

--- pydmt/helpers/project.py
+++ pydmt/helpers/project.py
@@ -2,7 +2,7 @@ import os
 import os.path
 import importlib
 
-def get_project_name():
+def get_name():
     mod = importlib.import_module("config.project")
     if hasattr(mod, "name"):
         return getattr(mod, "name")
