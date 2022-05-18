b2a74331939467b8ed3fc724cb928e3131d1ea4f
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 10:39:59 +0300

    typo

--- pydmt/helpers/signature.py
+++ pydmt/helpers/signature.py
@@ -12,13 +12,13 @@ def get_project_year_started() -> int:
 
 
 def get_copyright_years_long():
-    project_year_started = get_projected_year_started()
+    project_year_started = get_project_year_started()
     general_current_year = datetime.datetime.now().year
     return ", ".join(map(str, range(int(project_year_started), general_current_year)))
 
 
 def get_copyright_years_short():
-    project_year_started = get_projected_year_started()
+    project_year_started = get_project_year_started()
     general_current_year = datetime.datetime.now().year
     if general_current_year == project_year_started:
         return general_current_year
