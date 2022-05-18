b768d8b158eff0eff95cdd611cc01dbc981f82a1
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:32:27 +0300

    added stuff to add

--- config/project.py
+++ config/project.py
@@ -1,11 +1,3 @@
-github_username = "veltzer"
-name = "pydmt"
-github_repo_name = name
-website = f"https://{github_username}.github.io/{name}"
-website_source = f"https://github.com/{github_username}/{name}"
-website_git = f"git://github.com/{github_username}/{name}.git"
-website_download_ppa = "https://launchpanet/~mark-veltzer/+archive/ubuntu/ppa"
-website_download_src = website_source
 description_short = "python dependency management tool"
 keywords = [
     "pydmt",
@@ -18,4 +10,3 @@ keywords = [
     "mvn",
 ]
 license_type = "MIT"
-year_started = 2017
