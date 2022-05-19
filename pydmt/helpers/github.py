79e82e8562832902560167e767e439309f1fb22e
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-20 00:24:10 +0300

    deal with missing config.github

--- pydmt/helpers/github.py
+++ pydmt/helpers/github.py
@@ -2,9 +2,12 @@ import importlib
 
 
 def get_workflows_platforms():
-    mod = importlib.import_module("config.github")
-    if hasattr(mod, "workflows_platforms"):
-        return getattr(mod, "workflows_platforms")
+    try:
+        mod = importlib.import_module("config.github")
+        if hasattr(mod, "workflows_platforms"):
+            return getattr(mod, "workflows_platforms")
+    except ModuleNotFoundError:
+        pass
     mod = importlib.import_module("default.github")
     if hasattr(mod, "workflows_platforms"):
         return getattr(mod, "workflows_platforms")
