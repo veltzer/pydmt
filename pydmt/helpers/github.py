3e5a2c9d86d575bb46aae9ce5c0891a7e49defc4
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-20 00:04:20 +0300

    bug

--- pydmt/helpers/github.py
+++ pydmt/helpers/github.py
@@ -2,10 +2,10 @@ import importlib
 
 
 def get_workflows_platforms():
-    mod = importlib.import_module("default.github")
+    mod = importlib.import_module("config.github")
     if hasattr(mod, "workflows_platforms"):
         return getattr(mod, "workflows_platforms")
-    mod = importlib.import_module("config.github")
+    mod = importlib.import_module("default.github")
     if hasattr(mod, "workflows_platforms"):
         return getattr(mod, "workflows_platforms")
     raise ValueError("cannot find worklows platformas")
