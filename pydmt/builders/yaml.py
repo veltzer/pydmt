c68c5f511fae4d61b8faeff22076463868f08409
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-11 09:36:25 +0300

    load all documents

--- pydmt/builders/yaml.py
+++ pydmt/builders/yaml.py
@@ -16,18 +16,19 @@ SCHEMA_URL_YAML = "schema_url_yaml"
 class BuilderYaml(OneSourceOneTarget):
     def build(self):
         with open(self.source) as yaml_stream:
-            data = yaml.safe_load(yaml_stream)
-            if isinstance(data, dict):
-                if METADATA in data:
-                    metadata = data[METADATA]
-                    if SCHEMA_URL_JSON in metadata:
-                        schema_url = data[METADATA][SCHEMA_URL_JSON]
-                        with urlopen(schema_url) as f:
-                            schema = json.loads(f.read())
-                            validate(data, schema)
-                    if SCHEMA_URL_YAML in metadata:
-                        schema_url = data[METADATA][SCHEMA_URL_YAML]
-                        with urlopen(schema_url) as f:
-                            schema = yaml.safe_load(f.read())
-                            validate(data, schema)
+            datas = yaml.safe_load_all(yaml_stream)
+            for data in datas:
+                if isinstance(data, dict):
+                    if METADATA in data:
+                        metadata = data[METADATA]
+                        if SCHEMA_URL_JSON in metadata:
+                            schema_url = data[METADATA][SCHEMA_URL_JSON]
+                            with urlopen(schema_url) as f:
+                                schema = json.loads(f.read())
+                                validate(data, schema)
+                        if SCHEMA_URL_YAML in metadata:
+                            schema_url = data[METADATA][SCHEMA_URL_YAML]
+                            with urlopen(schema_url) as f:
+                                schema = yaml.safe_load(f.read())
+                                validate(data, schema)
             mkdir_touch(self.target)
