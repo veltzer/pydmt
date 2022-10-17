5be0d9326d0a4445618dc60ca897e62faa6c73aa
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-17 03:19:35 +0300

    removed requirements, MANIFEST

--- pydmt/static.py
+++ pydmt/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.1.76"
+VERSION_STR = "0.1.77"
 DESCRIPTION = "python dependency management tool"
 APP_NAME = "pydmt"
 LOGGER_NAME = "pydmt"
