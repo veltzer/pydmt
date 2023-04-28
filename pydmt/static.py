54d19b6a98ce10efb751c3426ff052e63e54cef9
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-28 20:21:43 +0300

    new version

--- pydmt/static.py
+++ pydmt/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.1.85"
+VERSION_STR = "0.1.86"
 DESCRIPTION = "Python dependency management tool"
 APP_NAME = "pydmt"
 LOGGER_NAME = "pydmt"
