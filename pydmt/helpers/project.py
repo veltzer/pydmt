6917d3802882b20a7f36fb2273de4a43218a8cd4
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-28 07:29:28 +0300

    sphinx stuff

--- pydmt/helpers/project.py
+++ pydmt/helpers/project.py
@@ -1,10 +1,7 @@
-import os
-import os.path
 import importlib
 
 
 def get_name():
+    """ this gives you the name of the project, this must exist """
     mod = importlib.import_module("config.project")
-    if hasattr(mod, "name"):
-        return getattr(mod, "name")
-    return os.path.basename(os.getcwd())
+    return getattr(mod, "name")
