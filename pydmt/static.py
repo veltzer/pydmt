dc31f98d9396ce31bd00bb8139b63dd80bfd30b4
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-28 23:02:14 +0300

    removed stuff from ~

--- pydmt/static.py
+++ pydmt/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.1.86"
+VERSION_STR = "0.1.87"
 DESCRIPTION = "Python dependency management tool"
 APP_NAME = "pydmt"
 LOGGER_NAME = "pydmt"
