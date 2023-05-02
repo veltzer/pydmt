aab92dd82ecf6ecb8f8ba0769dc90a7fac59b9ec
Mark Veltzer <mark.veltzer@gmail.com>
2023-05-02 15:47:53 +0300

    added better git helpers

--- pydmt/helpers/composites.py
+++ pydmt/helpers/composites.py
@@ -1,4 +1,4 @@
 import pydmt.helpers.apt
 import pydmt.helpers.git
 
-deb_version = f"{pydmt.helpers.git.git_version}~{pydmt.helpers.apt.codename}"
+deb_version = f"{pydmt.helpers.git.get_git_version()}~{pydmt.helpers.apt.codename}"
--- pydmt/helpers/git.py
+++ pydmt/helpers/git.py
@@ -1,9 +1,27 @@
 import subprocess
 
-git_last_tag = subprocess.check_output(
-    ["git", "describe", "--abbrev=0", "--tags"], stderr=subprocess.DEVNULL
-).decode().rstrip()
-git_describe = subprocess.check_output(
-    ["git", "describe"], stderr=subprocess.DEVNULL
-).decode().rstrip()
-git_version = ".".join(git_describe.split("-"))
+
+def get_git_last_tag() -> str:
+    """ return the output of git describe --tag """
+    return subprocess.check_output(
+        ["git", "describe", "--abbrev=0", "--tags"],
+        stderr=subprocess.DEVNULL,
+    ).decode().rstrip()
+
+
+def get_git_describe() -> str:
+    """ return the output of git describe """
+    return subprocess.check_output(
+        ["git", "describe"],
+        stderr=subprocess.DEVNULL,
+    ).decode().rstrip()
+
+
+def get_git_version() -> str:
+    """ get version by git """
+    return ".".join(get_git_describe().split("-"))
+
+
+def count_files(pattern: str) -> int:
+    """ Count the number of sources files of a certain pattern """
+    return int(subprocess.check_output(f"git ls-files -- {pattern} | wc -l").decode().rstrip())
