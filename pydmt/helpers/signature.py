058212cc2d6254cd2dc9ece83978c09a8ecf636a
Mark Veltzer <mark.veltzer@gmail.com>
2023-10-10 21:10:19 +0300

    added option to control repo location

--- pydmt/helpers/signature.py
+++ pydmt/helpers/signature.py
@@ -4,22 +4,22 @@ import time
 import git
 
 
-def get_project_year_started() -> int:
-    repo = git.Repo(".")
+def get_project_year_started(repo=".") -> int:
+    repo = git.Repo(repo)
     commits = list(repo.iter_commits())
     last_commit = commits[-1]
     t = time.gmtime(last_commit.authored_date)
     return t.tm_year
 
 
-def get_copyright_years_long():
-    project_year_started = get_project_year_started()
+def get_copyright_years_long(repo="."):
+    project_year_started = get_project_year_started(repo=repo)
     general_current_year = datetime.datetime.now().year
     return ", ".join(map(str, range(int(project_year_started), general_current_year + 1)))
 
 
-def get_copyright_years_short():
-    project_year_started = get_project_year_started()
+def get_copyright_years_short(repo="."):
+    project_year_started = get_project_year_started(repo=repo)
     general_current_year = datetime.datetime.now().year
     if general_current_year == project_year_started:
         return general_current_year
