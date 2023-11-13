8249d3caa36425951857629b63f9d85baa90bf48
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-13 10:33:29 +0200

    added gem feature

---
+++ pydmt/builders/gem.py
@@ -0,0 +1,20 @@
+"""
+This module installs gem modules
+"""
+
+
+from pydmt.utils.filesystem import unlink_files, mkdir_touch
+from pydmt.utils.subprocess import check_call
+
+from pydmt.api.one_source_one_target import OneSourceOneTarget
+
+
+class Installer(OneSourceOneTarget):
+    def build(self) -> None:
+        unlink_files([self.target])
+        args = [
+            'bundle',
+            'install',
+        ]
+        check_call(args)
+        mkdir_touch(self.target)
