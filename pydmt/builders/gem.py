cd0340270bf064d11bd16be2c0ed18e5e49431ed
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-13 17:30:00 +0200

    install in gems

--- pydmt/builders/gem.py
+++ pydmt/builders/gem.py
@@ -3,6 +3,7 @@ This module installs gem modules
 """
 
 
+import os
 from pydmt.utils.filesystem import unlink_files, mkdir_touch
 from pydmt.utils.subprocess import check_call
 
@@ -12,6 +13,7 @@ from pydmt.api.one_source_one_target import OneSourceOneTarget
 class Installer(OneSourceOneTarget):
     def build(self) -> None:
         unlink_files([self.target])
+        os.environ["GEM_HOME"] = "gems"
         args = [
             'bundle',
             'install',
