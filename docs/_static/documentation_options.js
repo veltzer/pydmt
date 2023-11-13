ac806b4be0a05f4ab8b5dfec25028d24b951d465
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-13 10:34:19 +0200

    new version

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.95',
+    VERSION: '0.1.96',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pydmt",
-    version="0.1.95",
+    version="0.1.96",
     packages=[
         "pydmt",
         "pydmt.api",
