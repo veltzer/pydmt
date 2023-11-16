e0b4f92896aa650d70596fbfff590e699026267e
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-16 06:43:29 +0200

    no commit message given

--- pydmt/static.py
+++ pydmt/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.1.98"
+VERSION_STR = "0.1.99"
 DESCRIPTION = "Python dependency management tool"
 APP_NAME = "pydmt"
 LOGGER_NAME = "pydmt"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pydmt",
-    version="0.1.98",
+    version="0.1.99",
     packages=[
         "pydmt",
         "pydmt.api",
