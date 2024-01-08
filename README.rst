2dd82b715e0b92ae97e9bc20877027f601065bc0
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-08 19:17:48 +0200

    new version

--- README.rst
+++ README.rst
@@ -12,6 +12,6 @@ project website: https://veltzer.github.io/pydmt
 
 author: Mark Veltzer
 
-version: 0.3.3
+version: 0.3.4
 
 	Mark Veltzer <mark.veltzer@gmail.com>, Copyright © 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pydmt",
-    version="0.3.3",
+    version="0.3.4",
     packages=[
         "pydmt",
         "pydmt.api",
