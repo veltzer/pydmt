3933483d1337bee0be01e6b9add780c897cd4ead
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-12 15:07:03 +0200

    more python.py changes

--- config/python.py
+++ config/python.py
@@ -21,7 +21,7 @@ install_requires: List[str] = [
     "venv-run",
     "gitpython",
 ]
-make_requires: List[str] = [
+build_requires: List[str] = [
     "pymakehelper",
     "pydmt",
     "types-jsonschema",
@@ -35,4 +35,4 @@ test_requires: List[str] = [
     "mypy",
     "types-PyYAML",
 ]
-requires = config_requires + install_requires + make_requires + test_requires
+requires = config_requires + install_requires + build_requires + test_requires
