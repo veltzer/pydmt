e1e615ab444096c5e9778a93c909d69b4b43a139
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-06 15:46:12 +0300

    no commit message given

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pydmt",
-    version="0.3.9",
+    version="0.3.10",
     packages=[
         "pydmt",
         "pydmt.api",
