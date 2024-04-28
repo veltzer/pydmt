2a87e9fa606e52747777db8dbb22545f8c924c56
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-28 10:06:11 +0300

    no commit message given

---
+++ pydmt/helpers/attrs.py
@@ -0,0 +1,30 @@
+import importlib
+
+from pydmt.utils.importlib import module_exists
+
+
+def get_github_username():
+    if not module_exists("config.personal"):
+        return None
+    mod = importlib.import_module("config.personal")
+    if not hasattr(mod, "github_username"):
+        return None
+    return getattr(mod, "github_username")
+
+
+def get_launchpad_username():
+    if not module_exists("config.personal"):
+        return None
+    mod = importlib.import_module("config.personal")
+    if not hasattr(mod, "launchpad_username"):
+        return None
+    return getattr(mod, "launchpad_username")
+
+
+def get_deps():
+    if not module_exists("config.deps"):
+        return None
+    mod = importlib.import_module("config.deps")
+    if not hasattr(mod, "packages"):
+        return None
+    return getattr(mod, "packages")
--- pydmt/helpers/urls.py
+++ pydmt/helpers/urls.py
@@ -1,50 +1,39 @@
-import importlib
-
 import pydmt.helpers.project
-from pydmt.utils.importlib import module_exists
-
-
-def get_github_username():
-    mod = importlib.import_module("config.personal")
-    if hasattr(mod, "github_username"):
-        return getattr(mod, "github_username")
-    return None
-
-
-def get_launchpad_username():
-    mod = importlib.import_module("config.personal")
-    if hasattr(mod, "launchpad_username"):
-        return getattr(mod, "launchpad_username")
-    return None
+from pydmt.helpers.attrs import get_github_username
 
 
 def get_website():
     github_username = get_github_username()
+    if github_username is None:
+        return None
     name = pydmt.helpers.project.get_name()
+    if name is None:
+        return None
     return f"https://{github_username}.github.io/{name}"
 
 
 def get_website_source():
     github_username = get_github_username()
+    if github_username is None:
+        return None
     name = pydmt.helpers.project.get_name()
+    if name is None:
+        return None
     return f"https://github.com/{github_username}/{name}"
 
 
 def get_website_git():
     github_username = get_github_username()
+    if github_username is None:
+        return None
     name = pydmt.helpers.project.get_name()
+    if name is None:
+        return None
     return f"git://github.com/{github_username}/{name}.git"
 
 
 def get_website_ppa():
     launchpad_username = get_github_username()
-    return f"https://launchpanet/~{launchpad_username}/+archive/ubuntu/ppa"
-
-
-def get_deps():
-    if module_exists("config.deps"):
-        mod = importlib.import_module("config.deps")
-        if hasattr(mod, "packages"):
-            return getattr(mod, "packages")
+    if launchpad_username is None:
         return None
-    return None
+    return f"https://launchpanet/~{launchpad_username}/+archive/ubuntu/ppa"
