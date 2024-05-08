79aecc9507ae3ee7ea04f54b123b328ca1ecd342
Mark Veltzer <mark.veltzer@gmail.com>
2024-05-08 10:45:33 +0300

    new version

--- pydmt/static.py
+++ pydmt/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.3.15"
+VERSION_STR = "0.3.16"
 DESCRIPTION = "Python dependency management tool"
 APP_NAME = "pydmt"
 LOGGER_NAME = "pydmt"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pydmt",
-    version="0.3.15",
+    version="0.3.16",
     packages=[
         "pydmt",
         "pydmt.api",
