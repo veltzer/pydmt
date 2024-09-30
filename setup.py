6bc3cc78e974049b91e4b0c97437618abf7b33c1
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-30 17:47:24 +0300

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_readme():
 setuptools.setup(
     # the first three fields are a must according to the documentation
     name="pydmt",
-    version="0.3.16",
+    version="0.3.17",
     packages=[
         "pydmt",
         "pydmt.api",
